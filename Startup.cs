61820e98df498b5032a16fe8f8251f50ca0a63f8
tuliopaim <tutpaim@gmail.com>
2020-12-13 18:12:36 -0300

    Cria metodo para Configurar as injeções de dependencia

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
@@ -39,36 +40,7 @@ namespace Multimidia.Api
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Multimidia.Api", Version = "v1" });
             });
 
-            var appSettingsSection = _configuration.GetSection("AppSettings");
-            services.Configure<AppSettings>(appSettingsSection);
-            var appSettings = appSettingsSection.Get<AppSettings>();
-
-            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
-
-            services.AddAuthentication(x =>
-            {
-                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(x =>
-            {
-                x.RequireHttpsMetadata = false;
-                x.SaveToken = true;
-                x.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(key),
-                    ValidateIssuer = false,
-                    ValidateAudience = false
-                };
-            });
-
-            services.AddScoped<AuthService>();
-            services.AddScoped<TokenService>();
-            services.AddTransient<FileService>();
-
-            services.AddScoped<IUserRepository, UserRepository>();
-            services.AddScoped<IVideoRepository, VideoRepository>();
+            ConfigurarServicos(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -91,13 +63,52 @@ namespace Multimidia.Api
                 .AllowAnyHeader());
 
             app.UseAuthentication();
-            app.UseAuthorization();
-
+            app.UseAuthorization(); 
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private void ConfigurarServicos(IServiceCollection services)
+        {
+            /* Obtem segredo do appsettings.json */
+
+            var appSettingsSection = _configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+            var appSettings = appSettingsSection.Get<AppSettings>();
+
+            /* configura jwt bearer */
+
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            /* injeção de dependencia */
+
+            services.AddScoped<AuthService>();
+            services.AddScoped<TokenService>();
+            services.AddTransient<FileService>();
+
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IVideoRepository, VideoRepository>();
         }
     }
 }
